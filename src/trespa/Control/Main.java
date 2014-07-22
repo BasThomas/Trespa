6dbcd3c2b114540c0c173189991a0d3f26ba87e0
Bas Thomas Broek <bas@basbroek.nl>
2014-07-22 11:47:19 +0200

    Removed printTest

--- src/trespa/Control/Main.java
+++ src/trespa/Control/Main.java
@@ -23,45 +23,5 @@ public class Main
     {
         LaunchGUI gui = new LaunchGUI();
         gui.setVisible(true);
-        
-        //printTest();
-    }
-    
-    public static void printTest()
-    {
-        Database database = new Database();
-        
-        List<ShippingPoint> shippingPoints = database.getShippingPoints();
-        List<Customer> customers = database.getCustomers();
-        List<Truck> trucks = database.getTrucks();
-        List<Placement> placements = database.getPossiblePlacementsForRoute(null, null);
-        
-        System.out.printf("\n#### Shipping points ####\n");
-        
-        for(ShippingPoint sp : shippingPoints)
-        {
-            System.out.printf(sp.toString() + "\n");
-        }
-        
-        System.out.printf("\n#### Customers ####\n");
-        
-        for(Customer c : customers)
-        {
-            System.out.printf(c.toString() + "\n");
-        }
-        
-        System.out.printf("\n#### Trucks ####\n");
-        
-        for(Truck t : trucks)
-        {
-            System.out.printf(t.toString() + "\n");
-        }
-        
-        System.out.printf("\n#### Placements ####\n");
-        
-        for(Placement p : placements)
-        {
-            System.out.printf(p.toString() + "\n");
-        }
     }
 }
